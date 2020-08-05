699beaaf29ab47bc5886066b4912389ab0a21f9a
Chris Eckhardt <ceckhardt06@gmail.com>
2020-08-05 15:07:52 -0700

    Encryption function implemented, comments added, main function opens and reads data.txt

--- src/com/chriseckhardt/Main.java
+++ src/com/chriseckhardt/Main.java
@@ -20,15 +20,46 @@ public class Main {
     }
 
 
+    /************************
+     * Encrypt() will process a string as input,
+     * build and return a resulting encrypted version
+     * of the input string.
+     * @param str
+     * @return encrypted string of str
+     ************************/
 
     private static String encrypt(String str) {
 
+        String result = "";
+
+        /* STEP 1: remove spaces */
         str = str.replaceAll("\\s+", "");
+
+        /* STEP 2: calculate dimensions */
         double dim = Math.sqrt(str.length());
         int rows = (int) Math.floor(dim);
         int columns = (int) Math.ceil(dim);
-        char[][] arr = new char[rows][columns];
-        return "";
+
+        /* STEP 3: check dimensions */
+        if(columns*rows < str.length()) {
+            if(columns > rows) {
+                rows = columns;
+            } else {
+                columns = rows;
+            }
+        }
+
+        /* STEP 4: build resulting string from column and index offset */
+        for(int i = 0; i < columns; i++) {
+            for(int j = 0; j < rows; j++) {
+                if(j*columns+i < str.length())
+                    result += str.charAt(j*columns+i);
+            }
+            if(i < columns-1)
+                result += " ";
+        }
+
+        return result;
     }
 
 }
